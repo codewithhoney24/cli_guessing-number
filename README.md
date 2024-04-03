3a6b6e3b051f2b09c1f5cf9401657d9824389f2b
NOUSHEEN ATIF <161060487+codewithhoney24@users.noreply.github.com>
2024-04-03 12:37:12 -0700

    push my cli guessing number to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# cli_guessing-number" 
---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.blue("\n\tWelcome to code-with-honey Cli Number Guessing Game\n"));
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "(Enter your guess number (Number limit from 1 to 6) :"
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log(chalk.green("\n\tCongratulation! You Guess a Correct Number.\n"));
+}
+else {
+    console.log(chalk.red("\n\tSorry! You Guess a Wrong Number.\n"));
+}
